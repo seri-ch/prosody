3d0f553ed75a3cb5dad1ba8c8374b036162f2ef7
seri.ch <seriatham@gmail.com>
2026-08-08 19:33:58 +0300

    7

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-// @ts-expect-error CSS imports are handled by Next.js
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 
